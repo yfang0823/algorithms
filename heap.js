04653f4562f4d3f7f79544af1d3d4ee58d1921c7
yfang <yfang0823@163.com>
2017-01-19 23:49:57 +0800

    最大堆

---
+++ heap.js
@@ -0,0 +1,18 @@
+/*
+  1，获取左、右子树根节点下标函数 done
+  2，维护最大推性质函数
+  3，建立最大堆
+  4，堆排序
+*/
+
+var left = function(index) {
+    return 2 * index
+}
+
+var right = function(index) {
+    return 2 * index + 1
+}
+
+var maxHeapify(array, index) {
+    
+}
